c6add45819d45dc1226e8cb820df133bc2079c24
pratik.prasad <pratik.prasad@accenture.com>
2018-08-09 17:29:33 -0400

    UI Changes Complete Not Tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { DataService } from '../services/data-service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  data = '';
+  tags = [
+    'Res./Mail. Address',
+    'Tel. Number',
+    'Email Addr.',
+    'Date',
+    'First Name',
+    'Last Name',
+    'Title',
+    'Org. Addr.',
+    'Sender Name',
+    'Sender Signature'
+  ];
+
+  ngOnInit(){
+    this.service.getData().subscribe( data=> {
+      this.data = this.trimDocData(data);
+    })
+  }
+
+  trimDocData(data: string): string {
+    const docEle = document.createElement('div');
+    docEle.innerHTML = data;
+    
+    return docEle.getElementsByClassName('3DWordSection1')[0].innerHTML;
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatTabsModule} from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataService } from '../services/data-service';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +16,17 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTabsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatGridListModule,
+    MatListModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
